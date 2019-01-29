2068be69c54cb33e18f4100edb7d442d61ea0486
Anton Bossenbroek <anton.bossenbroek@me.com>
2019-01-29 13:28:50 -0500

    Implements basic test code for data loading

--- src/main/scala/observatory/Main.scala
+++ src/main/scala/observatory/Main.scala
@@ -1,6 +1,10 @@
 package observatory
 
 object Main extends App {
+  import org.apache.log4j.{Level, Logger}
+  Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
 
-
+ val temp1989 = Extraction.locateTemperatures(1989,
+   "src/main/resources/stations.csv",
+   "src/main/resources/[12]*.csv")
 }
