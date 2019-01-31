54d47bdd0fceec9edf975e3d0708dcf03505c9e9
Anton Bossenbroek <anton.bossenbroek@me.com>
2019-01-30 21:31:06 -0500

    Implements test code in main

--- src/main/scala/observatory/Main.scala
+++ src/main/scala/observatory/Main.scala
@@ -6,5 +6,9 @@ object Main extends App {
 
  val temp1989 = Extraction.locateTemperatures(1989,
    "src/main/resources/stations.csv",
-   "src/main/resources/[12]*.csv")
+  // "src/main/resources/[12]*.csv")
+  "src/main/resources/198[89].csv")
+
+  println(temp1989.head)
+  Extraction.locationYearlyAverageRecords(temp1989)
 }
