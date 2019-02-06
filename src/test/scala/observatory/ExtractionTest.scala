cbbb3a631aa19eaf93ffe4c42cd93dfefc2e6d5f
Anton Bossenbroek <anton.bossenbroek@me.com>
2019-02-06 19:27:47 +0000

    Implements tests of extraction steps

--- src/test/scala/observatory/ExtractionTest.scala
+++ src/test/scala/observatory/ExtractionTest.scala
@@ -1,10 +1,33 @@
 package observatory
 
-import org.junit.runner.RunWith
-import org.scalatest.FunSuite
-import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
-trait ExtractionTest extends FunSuite {
+trait ExtractionTest extends FunSuite with BeforeAndAfterAll {
+  import org.apache.log4j.{Level, Logger}
 
-  
+  Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+
+  def initializeExtraction(): Boolean =
+    try {
+      Extraction
+      true
+    } catch {
+      case ex: Throwable =>
+        println(ex.getMessage)
+        ex.printStackTrace()
+        false
+    }
+
+  override def afterAll(): Unit = {
+    assert(initializeExtraction(), "cannot start spark")
+    import Extraction._
+    spark.stop()
+  }
+
+  test("temperature extraction works") {
+    assert(initializeExtraction(), "cannot start spark")
+    import Extraction._
+    val temps = locateTemperatures(2000, "/stations.csv", "/temperature_test.csv")
+    assert(temps.size === 9, "Expected nine results")
+  }
 }
