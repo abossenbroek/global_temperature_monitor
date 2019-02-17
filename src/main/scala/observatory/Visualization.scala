9eda18c488660992ca03fa1b5cfeb032f6bcf3cc
Anton Bossenbroek <anton.bossenbroek@me.com>
2019-02-17 21:14:37 +0000

    Implements visualization temperature and color interpolation

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -2,6 +2,8 @@ package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
 
+import scala.collection.immutable.TreeMap
+
 /**
   * 2nd milestone: basic visualization
   */
@@ -13,7 +15,47 @@ object Visualization {
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
-    ???
+    // TODO: first determine whether our location is within 1km range of any point in the set
+
+    // Based on https://gis.stackexchange.com/questions/142326/calculating-longitude-length-in-miles
+    val kmPerDegree = 111d
+
+    def euclideanDistance(a: Location): Double = {
+      math.sqrt((a.lon - location.lon) * (a.lon - location.lon) + (a.lat - location.lat) * (a.lat - location.lat))
+    }
+
+    val euclidianDistances = temperatures
+      .par
+      .foldLeft(Map[Double, Temperature]())((map, key) => map + (euclideanDistance(key._1) -> key._2))
+
+    if (euclidianDistances.keys.min < 1 / kmPerDegree) {
+      euclidianDistances(euclidianDistances.keys.min)
+    } else {
+      val earthRadius = 6357 // km
+
+      def greatCircle(a: Location): Double =
+        if (a == location) 0
+        else if ((a.lon + location.lon).abs < 1 / kmPerDegree && (a.lat + location.lat).abs < 1 / kmPerDegree) math.Pi
+        else {
+          val absDiffLon = (math.toRadians(a.lon) - math.toRadians(location.lon)).abs
+
+          math.acos(math.sin(math.toRadians(a.lat)) * math.sin(math.toRadians(location.lat))
+            + math.cos(math.toRadians(a.lat)) * math.cos(math.toRadians(location.lat)) * math.cos(absDiffLon))
+        }
+
+      assert(greatCircle(location) < 1e-6)
+      assert((greatCircle(Location(-location.lat, -location.lon)) - math.Pi).abs < 1e-6)
+      assert((greatCircle(Location(location.lat, -location.lon)) - math.Pi).abs > 1e-6)
+
+      def distance(a: Location, p: Double = 1.5) = math.pow(earthRadius * greatCircle(a), -p)
+
+      val weights = temperatures.par.foldLeft(Map[Double, Temperature]())((map, key) => map + (distance(key._1) -> key._2))
+
+      val weigthNumerator = weights.par.foldLeft(0d)((acc, key) => acc + key._1 * key._2)
+      val weigthDenum = weights.par.foldLeft(0d)((acc, key) => acc + key._1)
+
+      weigthNumerator / weigthDenum
+    }
   }
 
   /**
@@ -22,7 +64,37 @@ object Visualization {
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Temperature, Color)], value: Temperature): Color = {
-    ???
+    def interpolator(color1: (Temperature, Color), color2: (Temperature, Color), toInterpolate: Temperature): Color = {
+      def linearInterpolation(y0: Int, y1: Int): Int = {
+        val x0 = color1._1
+        val x1 = color2._1
+
+        val scale = (toInterpolate - x0) / (x1 - x0).toFloat
+        (y0 * (1f - scale) + y1 * scale).round.toInt
+      }
+
+      val redComp = linearInterpolation(color1._2.red, color2._2.red)
+      val greenComp = linearInterpolation(color1._2.green, color2._2.green)
+      val blueComp = linearInterpolation(color1._2.blue, color2._2.blue)
+
+      Color(redComp, greenComp, blueComp)
+    }
+
+    val scale = points.toSeq.par
+        .foldLeft(TreeMap[Temperature, Color]())((map, key) =>
+          map + (key._1 -> key._2))
+
+    if (value >= scale.keys.max) {
+      scale(scale.keys.max)
+    } else if (value <= scale.keys.min) {
+      scale(scale.keys.min)
+    } else {
+      val closest = Seq(scale.to(value).lastOption, scale.from(value).headOption)
+        .flatten
+        .distinct
+
+      interpolator(closest.head, closest.tail.head, value)
+    }
   }
 
   /**
@@ -31,7 +103,10 @@ object Visualization {
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
   def visualize(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)]): Image = {
-    ???
+    val width = 360
+    val height = 180
+    val output = Array[Pixel](width * height)
+    Image(width, height, output)
   }
 
 }
--- src/test/scala/observatory/VisualizationTest.scala
+++ src/test/scala/observatory/VisualizationTest.scala
@@ -5,6 +5,39 @@ import org.scalatest.FunSuite
 import org.scalatest.prop.Checkers
 
 trait VisualizationTest extends FunSuite with Checkers {
+  import Visualization._
+
+  test("Test color is properly interpolated") {
+    val interMediateColor = interpolateColor(List((-1.0, Color(255, 0, 0)), (0, Color(0, 0, 255))), -0.5)
+    assert(interMediateColor === Color(128, 0, 128))
+  }
+
+  test("Exceeding the color scale should give the largest color") {
+    val maxColor = interpolateColor(List((0.0, Color(255, 0, 0)), (1.0, Color(0, 0, 255))), 1.5)
+    assert(maxColor === Color(0, 0, 255), "Maximum color should be extrapolated")
+    val minColor = interpolateColor(List((0.0, Color(255, 0, 0)), (1.0, Color(0, 0, 255))), -0.5)
+    assert(minColor === Color(255, 0, 0), "Minimum color should be extrapolated")
+  }
+
+  test("More advanced color scaling") {
+    val intermediateColor = interpolateColor(List((-2.147483648E9, Color(255,0,0)), (0.0,Color(0,0,255))), -1.610612736E9)
+    assert(intermediateColor === Color(191, 0, 64))
+  }
+
+  test("If locations are less than 1km apart temperaturePrediction should return that temperature") {
+    val temps = List((Location(48.6598091, -114.1260281), 20d), (Location(48.6598091, 114.1260281), 30d))
+    val interpolTemp = predictTemperature(temps, Location(48.6598091, -114.1260281 + 110 / 111))
+    assert(interpolTemp === 20)
+  }
+
+  test("predicted temperature at location z should be closer to known temperature at location x than to known temperature at location y, if z is closer (in distance) to x than y, and vice versa") {
+    val glacierLoc = Location(48.6598091, -114.1260281)
+    val amsterdamLoc = Location(52.3546274, 4.8285839)
+    val londonLoc = Location(51.5285582, -0.2416796)
+    val temps = List((glacierLoc, -10d), (amsterdamLoc, 5d))
+    val interpolTemp = predictTemperature(temps, londonLoc)
+    assert((interpolTemp - temps(0)._2).abs > (interpolTemp - temps(1)._2).abs)
+  }
 
 
 }
