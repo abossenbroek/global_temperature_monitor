14d93a420679e9774b9c851e9e0bd6bc46b65595
Anton Bossenbroek <anton.bossenbroek@me.com>
2019-12-17 17:27:40 +0000

    Addition of visualizations tests

--- src/test/scala/observatory/VisualizationTest.scala
+++ src/test/scala/observatory/VisualizationTest.scala
@@ -1,10 +1,14 @@
 package observatory
 
 
-import org.scalatest.FunSuite
+import java.io.File
+
+import com.sksamuel.scrimage.Position.TopLeft
+import org.scalatest.{FunSuite, Matchers}
 import org.scalatest.prop.Checkers
 
-trait VisualizationTest extends FunSuite with Checkers {
+trait VisualizationTest extends FunSuite with Checkers with Matchers {
+
   import Visualization._
 
   test("Test color is properly interpolated") {
@@ -20,7 +24,7 @@ trait VisualizationTest extends FunSuite with Checkers {
   }
 
   test("More advanced color scaling") {
-    val intermediateColor = interpolateColor(List((-2.147483648E9, Color(255,0,0)), (0.0,Color(0,0,255))), -1.610612736E9)
+    val intermediateColor = interpolateColor(List((-2.147483648E9, Color(255, 0, 0)), (0.0, Color(0, 0, 255))), -1.610612736E9)
     assert(intermediateColor === Color(191, 0, 64))
   }
 
@@ -37,6 +41,25 @@ trait VisualizationTest extends FunSuite with Checkers {
     val temps = List((glacierLoc, -10d), (amsterdamLoc, 5d))
     val interpolTemp = predictTemperature(temps, londonLoc)
     assert((interpolTemp - temps(0)._2).abs > (interpolTemp - temps(1)._2).abs)
+  }
+
+  test("Coursera Scale and values 1") {
+    val temps = List((Location(45.0, -90.0), 10.0), (Location(-45.0, 0.0), 20.0))
+    val colScheme = List((10.0, Color(255, 0, 0)), (20.0, Color(0, 0, 255)))
+
+    val img = visualize(temps, colScheme)
+
+    img.output(new File("target/coursera_full1.png"))
+  }
+
+  test("Coursera Scale and values 2") {
+    val temps = Array((Location(45.0, -90.0), 20.0), (Location(45.0, 90.0), 0.0), (Location(0.0, 0.0), 10.0),
+      (Location(-45.0, -90.0), 0.0), (Location(-45.0, 90.0), 20.0))
+    val colScheme = List((0.0, Color(255, 0, 0)), (10.0, Color(0, 255, 0)), (20.0, Color(0, 0, 255)))
+
+    val img = visualize(temps, colScheme)
+
+    img.output(new File("target/coursera_full2.png"))
   }
 
 
